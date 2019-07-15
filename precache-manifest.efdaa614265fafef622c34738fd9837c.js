d4fb0df1106a4a415b114cfb723ba39eacbb59b3
sharmamayank221 <sharmamayank221@gmail.com>
2019-07-15 15:43:17 +0545

    Updates

---
+++ precache-manifest.efdaa614265fafef622c34738fd9837c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad724b3867b4e3a2b06d2a956c6426b",
+    "url": "/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6f622d9b63e0af0215b7",
+    "url": "/github.io/monsters-rolodex/static/css/main.9902e926.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/github.io/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6f622d9b63e0af0215b7",
+    "url": "/github.io/monsters-rolodex/static/js/main.38b7aaf6.chunk.js"
+  },
+  {
+    "revision": "1ac109da61bf9b161c11",
+    "url": "/github.io/monsters-rolodex/static/js/runtime~main.decfa424.js"
+  }
+]);
